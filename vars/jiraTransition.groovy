93a168b6548fcbce555defb0aa8ff91625b5cce4
Cris Apolinario <cris@cesar.org.br>
2019-05-27 18:45:29 +0200

    agent commit

--- vars/jiraTransition.groovy
+++ vars/jiraTransition.groovy
@@ -1,20 +1,4 @@
-// def jiraTransitions(gitlabSourceBranch, jiraTargetState){
-//     def JIRA_STATUS_TODO = '11'
-//     def JIRA_STATUS_INPROGRESS = '21'
-//     def JIRA_STATUS_AUTOMATEDTESTS = '81'
-//     def JIRA_STATUS_INREVIEW = '51'
-//     def JIRA_STATUS_READYFORTESTS = '61'
-//     def JIRA_STATUS_MANUALTESTS = '71'
-//     def JIRA_STATUS_DONE = '31'
-//     jiraTransitionIssue(
-//         idOrKey: gitlabSourceBranch,
-//         input: [
-//             transition: [ id: jiraTargetState ]
-//         ],
-//         site: 'JIRA'
-//     )
-// }
-
 def call(Map config){
     echo "Ola ${config.login}"
+    sh LibraryResource 'jira/transition.sh'
 }
