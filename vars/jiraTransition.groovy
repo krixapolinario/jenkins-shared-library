021d1f168102280ed4bb620cf01652f467cb9143
Cris Apolinario <cris@cesar.org.br>
2019-05-27 19:28:37 +0200

    agent commit

--- vars/jiraTransition.groovy
+++ vars/jiraTransition.groovy
@@ -22,7 +22,7 @@ def call(Map config){
     } else {
         jiraStateId='3'
     }
-    sh "echo >>>> ${jiraStateId}"
+    echo ">>>> ${jiraStateId}"
     // jiraTransitionIssue(
     //     idOrKey: config.jiraIssue,
     //     input: [
